9eff46243eb59f6d7ce111109ce73ae6f7e9a178
Daniel Scally <44758401+dascally@users.noreply.github.com>
2021-11-28 22:28:09 -0500

    Create header stub

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,25 @@
+import { Nav, Navbar } from 'react-bootstrap';
+
 export default function Header() {
-  return <header>I'm a header.</header>;
+  return (
+    <header>
+      <Navbar className='justify-content-around' expand='md' bg='secondary'>
+        <Navbar.Brand className='mx-4 me-lg-5' href='/'>
+          {/* TODO: add FA icon */}
+          Coffee & Comestibles
+        </Navbar.Brand>
+        {/* TODO: make this have nav-fill and navbar-collapse--hopefully with
+        idiomatic react-bootstrap, but custom classes are fine if needed */}
+        <Nav className='justify-content-center' fill>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/menu.html'>Menu</Nav.Link>
+          <Nav.Link href='events.html'>Events</Nav.Link>
+          <Nav.Link href='about.html'>About</Nav.Link>
+          <Nav.Link href='contact.html'>Contact</Nav.Link>
+          <div className='vr text-primary d-none d-md-block' />
+          <Navbar.Text>Login placeholder</Navbar.Text>
+        </Nav>
+      </Navbar>
+    </header>
+  );
 }
