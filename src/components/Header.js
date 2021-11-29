be70cedf65ff48ba8576801e042a6999cb55a574
Daniel Scally <44758401+dascally@users.noreply.github.com>
2021-11-29 16:51:01 -0500

    Add header/global nav content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Button, Col, Dropdown, Form, Nav, Navbar } from 'react-bootstrap';
 
 export default function Header() {
   return (
@@ -9,9 +9,10 @@ export default function Header() {
           {/* TODO: add FA icon */}
           Coffee & Comestibles
         </Navbar.Brand>
-        {/* TODO: make this have nav-fill and navbar-collapse--hopefully with
-        idiomatic react-bootstrap, but custom classes are fine if needed */}
-        <Nav className='justify-content-center' fill>
+        <Navbar.Collapse
+          id='globalNav'
+          className='justify-content-center navbar-nav nav-fill'
+        >
           {/* TODO: Either make a custom component wrapper to style router
           Links or find a library that does that for you */}
           <NavLink className='nav-link' to='/'>
@@ -22,8 +23,50 @@ export default function Header() {
           <Nav.Link href='about.html'>About</Nav.Link>
           <Nav.Link href='contact.html'>Contact</Nav.Link>
           <div className='vr text-primary d-none d-md-block' />
-          <Navbar.Text>Login placeholder</Navbar.Text>
-        </Nav>
+          <Dropdown className='mb-2 mb-md-0' as={Nav.Item}>
+            <Dropdown.Toggle as={Nav.Link}>Login</Dropdown.Toggle>
+            <Dropdown.Menu align='end' className='bg-light mb-2 shadow'>
+              <Form className='py-1 px-3'>
+                <Form.Group className='mb-2'>
+                  <Form.Label htmlFor='loginEmail' className='mb-1'>
+                    Email
+                  </Form.Label>
+                  <Form.Control type='email' name='email' id='loginEmail' />
+                </Form.Group>
+                <Form.Group className='mb-2'>
+                  <Form.Label htmlFor='loginPassword' className='mb-1'>
+                    Password
+                  </Form.Label>
+                  <Form.Control
+                    type='password'
+                    name='password'
+                    id='loginPassword'
+                  />
+                </Form.Group>
+                <Form.Group className='mb-2'>
+                  <Form.Check
+                    type='checkbox'
+                    name='remember'
+                    id='loginRemember'
+                    label='Remember me'
+                  />
+                </Form.Group>
+                <Form.Group className='d-flex justify-content-center'>
+                  <Button type='submit'>Log in</Button>
+                </Form.Group>
+              </Form>
+              <Dropdown.Divider />
+              <Dropdown.Item>New? Make an account.</Dropdown.Item>
+              <Dropdown.Item>Forgot password?</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Collapse>
+        <Col className='d-md-none' />
+        <Navbar.Toggle
+          className='mx-3'
+          label='Toggle navigation'
+          aria-controls='globalNav'
+        />
       </Navbar>
     </header>
   );
