f62107ad65e53794c1ba2497484a4f1b87ee5fe5
Daniel Scally <44758401+dascally@users.noreply.github.com>
2021-12-03 21:01:42 -0500

    Refactor Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,33 +18,11 @@ export default function Footer() {
           lg
           className='d-flex align-items-center justify-content-center text-nowrap'
         >
-          <address>
-            1234 Street Avenue
-            <br />
-            Townopolis, ST 54321
-          </address>
+          <FooterAddress />
         </Col>
         <div className='vr p-0 text-primary d-none d-sm-block' />
         <Col sm md={4} lg xl={3} className='text-nowrap'>
-          <Table size='sm' borderless>
-            <caption className='caption-top text-center text-reset'>
-              Hours
-            </caption>
-            <tbody>
-              <tr>
-                <th className='fw-light pe-2'>Monday-Friday</th>
-                <td>9am - 5pm</td>
-              </tr>
-              <tr>
-                <th className='fw-light'>Saturday</th>
-                <td>9am - 1pm</td>
-              </tr>
-              <tr>
-                <th className='fw-light'>Sunday</th>
-                <td>closed</td>
-              </tr>
-            </tbody>
-          </Table>
+          <FooterHours />
         </Col>
         <div className='vr p-0 text-primary d-none d-md-block' />
         <Col
@@ -52,37 +30,76 @@ export default function Footer() {
           lg
           className='d-flex flex-column align-items-center justify-content-center'
         >
-          <address className='lh-lg'>
-            <a href='mailto:contact@coffeeandcomestibles.com'>
-              <FontAwesomeIcon icon={faEnvelope} className='me-2' />
-              {/* <span class='bi-envelope me-2' aria-label='Email'></span> */}
-              contact@coffeeandcomestibles.com
-            </a>
-            <br />
-            <a href='tel:123-555-7890'>
-              <FontAwesomeIcon icon={faPhone} className='me-2' />
-              (123) 555-7890
-            </a>
-          </address>
-          <div className='fs-4 text-primary d-flex'>
-            <a href='#' className='link-primary me-4'>
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-            <a href='#' className='link-primary me-4'>
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a href='#' className='link-primary me-4'>
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <a
-              href='https://github.com/dascally/coffee-and-comestibles-react'
-              className='link-primary'
-            >
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-          </div>
+          <FooterContact />
         </Col>
       </Row>
     </footer>
+  );
+}
+
+function FooterAddress() {
+  return (
+    <address>
+      1234 Street Avenue
+      <br />
+      Townopolis, ST 54321
+    </address>
+  );
+}
+
+function FooterHours() {
+  return (
+    <Table size='sm' borderless>
+      <caption className='caption-top text-center text-reset'>Hours</caption>
+      <tbody>
+        <tr>
+          <th className='fw-light pe-2'>Monday-Friday</th>
+          <td>9am - 5pm</td>
+        </tr>
+        <tr>
+          <th className='fw-light'>Saturday</th>
+          <td>9am - 1pm</td>
+        </tr>
+        <tr>
+          <th className='fw-light'>Sunday</th>
+          <td>closed</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+function FooterContact() {
+  return (
+    <>
+      <address className='lh-lg'>
+        <a href='mailto:contact@coffeeandcomestibles.com'>
+          <FontAwesomeIcon icon={faEnvelope} className='me-2' />
+          {/* <span class='bi-envelope me-2' aria-label='Email'></span> */}
+          contact@coffeeandcomestibles.com
+        </a>
+        <br />
+        <a href='tel:123-555-7890'>
+          <FontAwesomeIcon icon={faPhone} className='me-2' />
+          (123) 555-7890
+        </a>
+      </address>
+      <div className='fs-4 text-primary d-flex'>
+        <a href='#' className='link-primary me-4'>
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href='#' className='link-primary me-4'>
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href='#' className='link-primary me-4'>
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          href='https://github.com/dascally/coffee-and-comestibles-react'
+          className='link-primary'
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
+    </>
   );
 }
