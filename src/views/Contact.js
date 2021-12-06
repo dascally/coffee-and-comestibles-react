096681ae0890bcb2bf66f0229fa97d08bca457e3
Daniel Scally <44758401+dascally@users.noreply.github.com>
2021-12-05 20:01:26 -0500

    Contact: Add feedback form

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -40,9 +40,107 @@ export default function Contact() {
         <Row className='justify-content-center'>
           <Col xl={10} xxl={8}>
             <h1 className='mb-sm-3'>Give us feedback!</h1>
+            <FeedbackForm />
           </Col>
         </Row>
       </Container>
     </>
+  );
+}
+
+function FeedbackForm() {
+  return (
+    <Form>
+      <Form.Group controlId='name' as={Row} className='mb-1 mb-sm-3'>
+        <Form.Label column sm={2}>
+          Name
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type='text' name='name' />
+        </Col>
+      </Form.Group>
+      <Form.Group controlId='email' as={Row} className='mb-1 mb-sm-3'>
+        <Form.Label column sm={2}>
+          Email
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type='email' name='email' />
+        </Col>
+      </Form.Group>
+      <Form.Group controlId='phone' as={Row} className='mb-1 mb-sm-3'>
+        <Form.Label column sm={2}>
+          Phone
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type='telephone' name='phone' />
+        </Col>
+      </Form.Group>
+      <fieldset className='row mb-1 mb-sm-3'>
+        <legend className='col-sm-2 col-form-label'>Rating</legend>
+        <Col sm={10} className='pt-sm-2'>
+          <Form.Check
+            inline
+            type='radio'
+            value='1'
+            label='1'
+            name='rating'
+            id='rating1'
+          />
+          <Form.Check
+            inline
+            type='radio'
+            value='2'
+            label='2'
+            name='rating'
+            id='rating2'
+          />
+          <Form.Check
+            inline
+            type='radio'
+            value='3'
+            label='3'
+            name='rating'
+            id='rating3'
+          />
+          <Form.Check
+            inline
+            type='radio'
+            value='4'
+            label='4'
+            name='rating'
+            id='rating4'
+          />
+          <Form.Check
+            inline
+            type='radio'
+            value='5'
+            label='5'
+            name='rating'
+            id='rating5'
+          />
+        </Col>
+      </fieldset>
+      <Form.Group controlId='feedback' as={Row} className='mb-3'>
+        <Form.Label column sm={2}>
+          Feedback
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            as='textarea'
+            name='feedback'
+            placeholder='What have we done well? What can we do better?'
+            required
+            rows={8}
+          />
+        </Col>
+      </Form.Group>
+      <Row>
+        <Col className='text-center'>
+          <Button type='submit' variant='primary'>
+            Submit
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   );
 }
