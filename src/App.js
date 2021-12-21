6ef2cb5c5088ad7d44213a64d3469040a65bc3e0
Daniel Scally <44758401+dascally@users.noreply.github.com>
2021-12-21 01:29:14 -0500

    Add UserContext for handling logged in state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.scss';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import UserContext from './utils/UserContext.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 
 function App(props) {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
   return (
-    <>
-      <Header className={props.headerClass} />
+    <UserContext.Provider value={loggedInUser}>
+      <Header className={props.headerClass} setLoggedInUser={setLoggedInUser} />
       <main className='m-3 position-relative'>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Button, Col, Dropdown, Form, Nav, Navbar } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMugHot } from '@fortawesome/free-solid-svg-icons';
+import UserContext from '../utils/UserContext.js';
 
 export default function Header(props) {
+  const loggedInUser = useContext(UserContext);
+
   return (
     <header {...props}>
       <Navbar className='justify-content-around' expand='md' bg='secondary'>
@@ -30,43 +34,50 @@ export default function Header(props) {
             Contact
           </NavLink>
           <div className='vr text-primary d-none d-md-block' />
-          <Dropdown className='mb-2 mb-md-0' as={Nav.Item}>
-            <Dropdown.Toggle as={Nav.Link}>Login</Dropdown.Toggle>
-            <Dropdown.Menu align='end' className='bg-light mb-2 shadow'>
-              <Form className='py-1 px-3'>
-                <Form.Group className='mb-2'>
-                  <Form.Label htmlFor='loginEmail' className='mb-1'>
-                    Email
-                  </Form.Label>
-                  <Form.Control type='email' name='email' id='loginEmail' />
-                </Form.Group>
-                <Form.Group className='mb-2'>
-                  <Form.Label htmlFor='loginPassword' className='mb-1'>
-                    Password
-                  </Form.Label>
-                  <Form.Control
-                    type='password'
-                    name='password'
-                    id='loginPassword'
-                  />
-                </Form.Group>
-                <Form.Group className='mb-2'>
-                  <Form.Check
-                    type='checkbox'
-                    name='remember'
-                    id='loginRemember'
-                    label='Remember me'
-                  />
-                </Form.Group>
-                <Form.Group className='d-flex justify-content-center'>
-                  <Button type='submit'>Log in</Button>
-                </Form.Group>
-              </Form>
-              <Dropdown.Divider />
-              <Dropdown.Item>New? Make an account.</Dropdown.Item>
-              <Dropdown.Item>Forgot password?</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          {loggedInUser ? (
+            <p>Hi {loggedInUser}!</p>
+          ) : (
+            <Dropdown className='mb-2 mb-md-0' as={Nav.Item}>
+              <Dropdown.Toggle as={Nav.Link}>Login</Dropdown.Toggle>
+              <Dropdown.Menu align='end' className='bg-light mb-2 shadow'>
+                <Form
+                  className='py-1 px-3'
+                  onSubmit={() => props.setLoggedInUser('Danny')}
+                >
+                  <Form.Group className='mb-2'>
+                    <Form.Label htmlFor='loginEmail' className='mb-1'>
+                      Email
+                    </Form.Label>
+                    <Form.Control type='email' name='email' id='loginEmail' />
+                  </Form.Group>
+                  <Form.Group className='mb-2'>
+                    <Form.Label htmlFor='loginPassword' className='mb-1'>
+                      Password
+                    </Form.Label>
+                    <Form.Control
+                      type='password'
+                      name='password'
+                      id='loginPassword'
+                    />
+                  </Form.Group>
+                  <Form.Group className='mb-2'>
+                    <Form.Check
+                      type='checkbox'
+                      name='remember'
+                      id='loginRemember'
+                      label='Remember me'
+                    />
+                  </Form.Group>
+                  <Form.Group className='d-flex justify-content-center'>
+                    <Button type='submit'>Log in</Button>
+                  </Form.Group>
+                </Form>
+                <Dropdown.Divider />
+                <Dropdown.Item>New? Make an account.</Dropdown.Item>
+                <Dropdown.Item>Forgot password?</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
         </Navbar.Collapse>
         <Col className='d-md-none' />
         <Navbar.Toggle
