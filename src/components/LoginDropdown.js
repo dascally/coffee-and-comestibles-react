664c338fcd0a219398809261ae37d41dca752cdd
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-01-08 21:15:30 -0500

    LoginDropdown: Add dropdown for logged in users

--- src/components/LoginDropdown.js
+++ src/components/LoginDropdown.js
@@ -1,21 +1,45 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Button, Dropdown, Form, Nav } from 'react-bootstrap';
 import UserContext from '../utils/UserContext.js';
 
 export default function LoginDropdown(props) {
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   return (
-    <>
+    <Dropdown
+      className={props.className}
+      as={Nav.Item}
+      show={showDropdown}
+      onToggle={(isOpen) => {
+        setShowDropdown(!showDropdown);
+      }}
+    >
       {loggedInUser ? (
-        <p>Hi {loggedInUser}!</p>
+        <>
+          <Dropdown.Toggle as={Nav.Link}>Account</Dropdown.Toggle>
+          <Dropdown.Menu align='end' className='bg-light mb-2 shadow'>
+            <Dropdown.Item>Hi, {loggedInUser}!</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Rewards</Dropdown.Item>
+            <Dropdown.Item>Favorite Orders</Dropdown.Item>
+            <Dropdown.Item>Account Settings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={() => setLoggedInUser(null)}>
+              Logout
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </>
       ) : (
-        <Dropdown className={props.className} as={Nav.Item}>
+        <>
           <Dropdown.Toggle as={Nav.Link}>Login</Dropdown.Toggle>
           <Dropdown.Menu align='end' className='bg-light mb-2 shadow'>
             <Form
               className='py-1 px-3'
-              onSubmit={() => setLoggedInUser('Danny')}
+              onSubmit={() => {
+                setLoggedInUser('Danny');
+                setShowDropdown(false);
+              }}
             >
               <Form.Group className='mb-2'>
                 <Form.Label htmlFor='loginEmail' className='mb-1'>
@@ -49,8 +73,8 @@ export default function LoginDropdown(props) {
             <Dropdown.Item>New? Make an account.</Dropdown.Item>
             <Dropdown.Item>Forgot password?</Dropdown.Item>
           </Dropdown.Menu>
-        </Dropdown>
+        </>
       )}
-    </>
+    </Dropdown>
   );
 }
