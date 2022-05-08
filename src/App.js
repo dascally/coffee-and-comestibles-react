3f0b512171085541392c9f1b07c84b3ebfc3e700
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-05-08 14:25:59 -0400

    Remove unused context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.scss';
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import UserContext from './utils/UserContext.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 
 function App(props) {
-  const getOrSetLoggedInUserPair = useState(null);
-
   return (
-    <UserContext.Provider value={getOrSetLoggedInUserPair}>
+    <>
       <Header className={props.headerClass} />
       <main className='m-3 position-relative'>
         <Outlet />
       </main>
       <Footer />
-    </UserContext.Provider>
+    </>
   );
 }
 
