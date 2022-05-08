6cd29b9e007ade50383c6c68a49a484a8a9b15a7
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-05-07 22:47:33 -0400

    Menu: Use menu data fetched from server

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,11 +1,31 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { changeItemQuantity } from '../features/order/orderSlice.js';
 import { Container, Row, Col, Accordion } from 'react-bootstrap';
 import MenuItemCard from '../components/MenuItemCard.js';
 import ShoppingBasket from '../components/ShoppingBasket.js';
 import { fetchMenu } from '../features/menu/menuSlice.js';
-import menuData from '../assets/menu-data.js';
+
+const structureMenuData = (flatMenuData) => {
+  const structuredMenuData = [];
+  flatMenuData.forEach((menuItem) => {
+    const sectionName = menuItem.menuSection;
+    let section = structuredMenuData.find(
+      (section) => section.sectionName === sectionName
+    );
+    if (!section) {
+      section = { sectionName, items: [] };
+      structuredMenuData.push(section);
+    }
+
+    const newMenuItem = { ...menuItem, image: { ...menuItem.image } };
+    newMenuItem.image.src = `${process.env.PUBLIC_URL}/menu/${newMenuItem.image.src}`;
+    delete newMenuItem.menuSection;
+
+    section.items.push(newMenuItem);
+  });
+  return structuredMenuData;
+};
 
 export default function Menu() {
   const dispatch = useDispatch();
@@ -14,13 +34,16 @@ export default function Menu() {
     dispatch(fetchMenu());
   }, [dispatch]);
 
+  const flatMenuData = useSelector((state) => state.menu);
+  const menu = useMemo(() => structureMenuData(flatMenuData), [flatMenuData]);
+
   return (
     <>
-      <ShoppingBasket menu={menuData} />
+      <ShoppingBasket menu={menu} />
       <Container as='section'>
         <h1>Menu / Order Online</h1>
-        <Accordion defaultActiveKey={menuData[0].sectionName}>
-          {menuData.map((section) => {
+        <Accordion defaultActiveKey={menu[0]?.sectionName}>
+          {menu.map((section) => {
             return (
               <Accordion.Item
                 eventKey={section.sectionName}
@@ -31,7 +54,7 @@ export default function Menu() {
                   <Row xs={{ cols: 1 }} lg={{ cols: 2 }} className='g-3'>
                     {section.items.map((item) => {
                       return (
-                        <Col key={item.id}>
+                        <Col key={item._id}>
                           <MenuItemCard
                             name={item.name}
                             description={item.description}
