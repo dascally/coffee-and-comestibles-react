fc2b82c4f270cd9c9b91775edac78dc1e7c54d97
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-05-14 14:17:10 -0400

    Style AddSavedPayment

--- src/components/AddSavedPayment.js
+++ src/components/AddSavedPayment.js
@@ -59,147 +59,152 @@ const AddSavedPayment = () => {
       </Button>
       <Collapse in={open} className='mt-2'>
         <form onSubmit={handleSubmit}>
-          <Row className='mb-1'>
-            <Col xs={5} sm={4} md={3} lg={2}>
-              <label htmlFor='cardNumber' className='text-nowrap'>
-                Card number
-              </label>
-            </Col>
-            <Col xs={7} sm={8} md={9} lg={10}>
-              <input
-                type='text'
-                id='cardNumber'
-                name='cardNumber'
-                value={cardNumber}
-                onChange={(evt) => {
-                  setCardNumber(evt.target.value);
-                }}
-                required
-                disabled
-              />
-            </Col>
-          </Row>
-          <Row className='mb-1'>
-            <Col xs={5} sm={4} md={3} lg={2}>
-              <label htmlFor='securityCode' className='text-nowrap'>
-                Security code
-              </label>
-            </Col>
-            <Col xs={7} sm={8} md={9} lg={10}>
-              <input
-                type='password'
-                id='securityCode'
-                name='securityCode'
-                value={securityCode}
-                onChange={(evt) => {
-                  setSecurityCode(evt.target.value);
-                }}
-                pattern='\d{3}'
-                title='3 digits'
-                required
-              />
-            </Col>
-          </Row>
-          <Row className='mb-1'>
-            <Col xs={5} sm={4} md={3} lg={2}>
-              <label htmlFor='billingName' className='text-nowrap'>
-                Billing name
-              </label>
-            </Col>
-            <Col xs={7} sm={8} md={9} lg={10}>
-              <input
-                type='text'
-                id='billingName'
-                name='billingName'
-                value={billingName}
-                onChange={(evt) => {
-                  setBillingName(evt.target.value);
-                }}
-                required
-              />
-            </Col>
-          </Row>
-          <Row className='mb-1'>
-            <Col xs={5} sm={4} md={3} lg={2}>
-              <label htmlFor='streetAddress' className='text-nowrap'>
-                Street address
-              </label>
-            </Col>
-            <Col xs={7} sm={8} md={9} lg={10}>
-              <input
-                type='text'
-                id='streetAddress'
-                name='streetAddress'
-                value={streetAddress}
-                onChange={(evt) => {
-                  setStreetAddress(evt.target.value);
-                }}
-                required
-              />
-            </Col>
-          </Row>
-          <Row className='mb-1'>
-            <Col xs={5} sm={4} md={3} lg={2}>
-              <label htmlFor='city' className='text-nowrap'>
-                City
-              </label>
-            </Col>
-            <Col xs={7} sm={8} md={9} lg={10}>
-              <input
-                type='text'
-                id='city'
-                name='city'
-                value={city}
-                onChange={(evt) => {
-                  setCity(evt.target.value);
-                }}
-                required
-              />
-            </Col>
-          </Row>
-          <Row className='mb-1'>
-            <Col xs={5} sm={4} md={3} lg={2}>
-              <label htmlFor='state' className='text-nowrap'>
-                State
-              </label>
-            </Col>
-            <Col xs={7} sm={8} md={9} lg={10}>
-              <input
-                type='text'
-                id='state'
-                name='state'
-                value={state}
-                onChange={(evt) => {
-                  setState(evt.target.value);
-                }}
-                required
-              />
-            </Col>
-          </Row>
-          <Row className='mb-1'>
-            <Col xs={5} sm={4} md={3} lg={2}>
-              <label htmlFor='zipCode' className='text-nowrap'>
-                Zip code
-              </label>
-            </Col>
-            <Col xs={7} sm={8} md={9} lg={10}>
-              <input
-                type='text'
-                id='zipCode'
-                name='zipCode'
-                value={zipCode}
-                onChange={(evt) => {
-                  setZipCode(evt.target.value);
-                }}
-                pattern='\d{5,}'
-                title='5 or more digits'
-                required
-              />
-            </Col>
-          </Row>
-          <Button variant='primary' type='submit' disabled={isSubmitting}>
-            Submit
-          </Button>
+          <fieldset className='border rounded p-2'>
+            <legend className='px-2 float-none' style={{ width: 'revert' }}>
+              <h5>Add a new payment method</h5>
+            </legend>
+            <Row className='mb-1'>
+              <Col xs={5} sm={4} md={3} lg={2}>
+                <label htmlFor='cardNumber' className='text-nowrap'>
+                  Card number
+                </label>
+              </Col>
+              <Col xs={7} sm={8} md={9} lg={10}>
+                <input
+                  type='text'
+                  id='cardNumber'
+                  name='cardNumber'
+                  value={cardNumber}
+                  onChange={(evt) => {
+                    setCardNumber(evt.target.value);
+                  }}
+                  required
+                  disabled
+                />
+              </Col>
+            </Row>
+            <Row className='mb-1'>
+              <Col xs={5} sm={4} md={3} lg={2}>
+                <label htmlFor='securityCode' className='text-nowrap'>
+                  Security code
+                </label>
+              </Col>
+              <Col xs={7} sm={8} md={9} lg={10}>
+                <input
+                  type='password'
+                  id='securityCode'
+                  name='securityCode'
+                  value={securityCode}
+                  onChange={(evt) => {
+                    setSecurityCode(evt.target.value);
+                  }}
+                  pattern='\d{3}'
+                  title='3 digits'
+                  required
+                />
+              </Col>
+            </Row>
+            <Row className='mb-1'>
+              <Col xs={5} sm={4} md={3} lg={2}>
+                <label htmlFor='billingName' className='text-nowrap'>
+                  Billing name
+                </label>
+              </Col>
+              <Col xs={7} sm={8} md={9} lg={10}>
+                <input
+                  type='text'
+                  id='billingName'
+                  name='billingName'
+                  value={billingName}
+                  onChange={(evt) => {
+                    setBillingName(evt.target.value);
+                  }}
+                  required
+                />
+              </Col>
+            </Row>
+            <Row className='mb-1'>
+              <Col xs={5} sm={4} md={3} lg={2}>
+                <label htmlFor='streetAddress' className='text-nowrap'>
+                  Street address
+                </label>
+              </Col>
+              <Col xs={7} sm={8} md={9} lg={10}>
+                <input
+                  type='text'
+                  id='streetAddress'
+                  name='streetAddress'
+                  value={streetAddress}
+                  onChange={(evt) => {
+                    setStreetAddress(evt.target.value);
+                  }}
+                  required
+                />
+              </Col>
+            </Row>
+            <Row className='mb-1'>
+              <Col xs={5} sm={4} md={3} lg={2}>
+                <label htmlFor='city' className='text-nowrap'>
+                  City
+                </label>
+              </Col>
+              <Col xs={7} sm={8} md={9} lg={10}>
+                <input
+                  type='text'
+                  id='city'
+                  name='city'
+                  value={city}
+                  onChange={(evt) => {
+                    setCity(evt.target.value);
+                  }}
+                  required
+                />
+              </Col>
+            </Row>
+            <Row className='mb-1'>
+              <Col xs={5} sm={4} md={3} lg={2}>
+                <label htmlFor='state' className='text-nowrap'>
+                  State
+                </label>
+              </Col>
+              <Col xs={7} sm={8} md={9} lg={10}>
+                <input
+                  type='text'
+                  id='state'
+                  name='state'
+                  value={state}
+                  onChange={(evt) => {
+                    setState(evt.target.value);
+                  }}
+                  required
+                />
+              </Col>
+            </Row>
+            <Row className='mb-1'>
+              <Col xs={5} sm={4} md={3} lg={2}>
+                <label htmlFor='zipCode' className='text-nowrap'>
+                  Zip code
+                </label>
+              </Col>
+              <Col xs={7} sm={8} md={9} lg={10}>
+                <input
+                  type='text'
+                  id='zipCode'
+                  name='zipCode'
+                  value={zipCode}
+                  onChange={(evt) => {
+                    setZipCode(evt.target.value);
+                  }}
+                  pattern='\d{5,}'
+                  title='5 or more digits'
+                  required
+                />
+              </Col>
+            </Row>
+            <Button variant='primary' type='submit' disabled={isSubmitting}>
+              Submit
+            </Button>
+          </fieldset>
         </form>
       </Collapse>
     </>
