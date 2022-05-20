12188c2e4c6c9d0b57f914fefc380f87cf5d1aac
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-05-20 17:27:00 -0400

    fix: Change strategy for setting Netlify env vars

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
-export let SERVICE_URL;
-
-if (process.env.NETLIFY) SERVICE_URL = process.env.REACT_APP_SERVICE_URL;
-else SERVICE_URL = process.env.REACT_APP_DEV_SERVICE_URL;
+export const SERVICE_URL =
+  process.env.REACT_APP_SERVICE_URL ?? process.env.REACT_APP_DEV_SERVICE_URL;
