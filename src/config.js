a7a41c2acbc593a58c257bdf7bc2ecf26ee6f1d2
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-05-20 19:45:31 -0400

    Change SERVICE_URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export const SERVICE_URL =
-  process.env.REACT_APP_SERVICE_URL ?? process.env.REACT_APP_DEV_SERVICE_URL;
+// export const SERVICE_URL =
+//   process.env.REACT_APP_SERVICE_URL ?? process.env.REACT_APP_DEV_SERVICE_URL;
+
+export const SERVICE_URL = process.env.REACT_APP_DEV_SERVICE_URL;
